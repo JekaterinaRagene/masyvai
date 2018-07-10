5cf2b7c23d95f3ea61b0e6fff688d8447d039f9f
Mikas <Mikas@Mikas-PC>
2018-07-10 18:03:58 +0300

    initial commit

--- post.php
+++ post.php
@@ -4,19 +4,16 @@ if (isset($_GET['skaiciai'])){
         $skaicius = explode(',' , $skaiciai);
         //print_r($skaicius) ;
         $result = 0;
-        $suma = array_sum($skaicius);                      
-        foreach ($skaicius as $value => $value) {
-            $result = $suma + $value;
-        }   
-        $vidurkis = $result / count($skaicius);
+        $suma = array_sum($skaicius);    
+ 
+        $vidurkis = $suma / count($skaicius);
         echo 'Vidutinė temperatūra:'. round($vidurkis, 2).' laipsniai';
 }
-
-        $sk = asort($skaicius);
-         print_r($sk);
         echo 'Viso nuoskaitų:'. count($skaicius);
         echo 'Dvi didžiausios temperatūros:';
         echo 'Dvi mažiausios temperaturos';
 
-
+        $sk = array($skaicius);
+        arsort($sk) ;
+        print_r($sk);
 ?>
--- post_vidurkis.php
+++ post_vidurkis.php
@@ -5,11 +5,7 @@ if (isset($_GET['skaiciai'])){
     //print_r($skaicius) ;
     $suma = 0;
     $suma = array_sum($skaicius);
-    echo $suma;
-        
-    foreach ($skaicius as $value => $value) {
-        $suma = $suma + $value;
-    }   
+    
     $vidurkis = $suma / count($skaicius);
     echo 'Vidutinė reiksme:'. round($vidurkis, 2).'<br>';
 }
