f0b3462aeb45f3441859c0e802faed666b79c50b
Mikas <Mikas@Mikas-PC>
2018-07-10 13:34:18 +0300

    initial commit

--- index.php
+++ index.php
@@ -1,12 +1,83 @@
 <?php
-print '<h2>Vidutinės temeraturos skaičiuoklė</h2>';
+$menesiuVardai = [1=>'Sausis',
+                        2=>'Vasaris', 3=>'Kovas',
+                        4=>'Balandis', 5=>'Gegužė',                        
+                        6=>'Birželis', 7=>'Liepa',                        
+                        8=>'Rugpjutis', 9=>'Rugsėjis',                       
+                        10=>'Spalis', 11=>'Lapkritis',                       
+                        12=>'Gruodis'];
+
+$menesiuDienos = array (1=>31, 2=>28, 3=>31,
+4=>30, 5=>31, 6=>30, 7=>31, 8=>31,
+9=>30,10=>31, 11=>30, 12=>31);
+
+$counter = 0;
+$pirmas = [];
+$suma = 0;
+
+foreach ($menesiuDienos as $menesioIndeksas => $dienuSkaicius) {
+    if ($dienuSkaicius == 31) {
+        $pirmas[] = $menesiuVardai[$menesioIndeksas];
+        $pirmasString = implode(',',$pirmas);
+        $counter++;
+    }
+}
+echo '<p>Metuose yra <span>' . $counter . '</span> menesiai turintys <span> 31 </span> diena(<span>' . $pirmasString . '</span>)</p>';
+ '<br>';              
+$counter = 0;
+$antras = [];
+foreach ($menesiuDienos as $menesioIndeksas => $dienuSkaicius) {
+    if ($dienuSkaicius == 30) {
+        $antras[] = $menesiuVardai[$menesioIndeksas];
+        $antrasString = implode(',', $antras);
+        $counter++;
+    }
+}
+echo '<p>Metuose yra <span>' . $counter . '</span> mėnesiai turintys <span> 30 </span> dienu(<span>' . $antrasString . '</span>)</p>';
+'<br>';
+
+$counter = 0;
+$pirmas = [];
+foreach ($menesiuDienos as $menesioIndeksas => $dienuSkaicius) {
+    if ($dienuSkaicius == 28) {
+        $pirmas[] = $menesiuVardai[$menesioIndeksas];
+        $antrasString = implode(',', $pirmas);
+        $counter++;
+    }
+} 
+echo '<p>Metuose yra <span>' . $counter . '</span> mėnuo turintis <span> 28 </span> dienas (<span>' . $pirmasString . '</span>)</p>';
+'<br>';
+
+$suma = 0;
+foreach ($menesiuDienos as $menesioIndeksas => $dienuSkaicius) {
+    $suma += $dienuSkaicius;
+}
+echo '<p>Viso metuose yra <span>' . $suma . '</span> dienos. </p>';
+
+foreach ($menesiuDienos as $menesioIndeksas => $dienuSkaicius) {
+    echo $menesiuVardai[$menesioIndeksas] . $menesioIndeksas . '' . $dienuSkaicius . '<br>';
+}
+
 ?>
-<html>
-        <form action='post.php' method='get'>
-        Įveskite temperaturos nuoskaitas (per kablelį):
-        <input type='text' name='skaiciai' value=''>
-        <br>
-        <input type='submit' name='Suskaičiuoti'>
-    </form>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        span {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+
+</body>
 </html>
+     
+
 
--- post_vidTemp.php
+++ post_vidTemp.php
@@ -3,16 +3,16 @@ if (isset($_GET['skaiciai'])){
         $skaiciai = $_GET['skaiciai'];
         $skaicius = explode(',' , $skaiciai);
         //print_r($skaicius) ;
-        $suma = 0;
+        $result = 0;
         $suma = array_sum($skaicius);                      
         foreach ($skaicius as $value => $value) {
-            $suma = $suma + $value;
+            $result = $suma + $value;
         }   
-        $vidurkis = $suma / count($skaicius);
+        $vidurkis = $result / count($skaicius);
         echo 'Vidutinė temperatūra:'. round($vidurkis, 2).' laipsniai';
 }
 
-        $sk = arsort($skaicius);
+        $sk = asort($skaicius);
          print_r($sk);
         echo 'Viso nuoskaitų:'. count($skaicius);
         echo 'Dvi didžiausios temperatūros:';
